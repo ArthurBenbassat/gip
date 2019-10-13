7f64ac4c6e7c3a213b3096a15a93ce3e1c50ae30
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-10-13 12:16:08 +0200

    head snipptes

--- sql/dbconnection.php
+++ sql/dbconnection.php
@@ -3,10 +3,9 @@
 require_once 'settings.php';
 
 $connection = mysqli_connect($host, $user, $password, $database);
-mysqli_set_charset($connection, "utf8");
+mygit sqli_set_charset($connection, "utf8");
 if(!$connection){
   echo "Error" . mysqli_connect_error();
   exit;
 }
 session_start();
-?>
