2c5078256e28e3c5f9604ae48f819da0533a92fc
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-10-28 21:43:35 +0100

    opkuis

--- sql/dbconnection.php
+++ sql/dbconnection.php
@@ -3,7 +3,7 @@
 require_once 'settings.php';
 
 $connection = mysqli_connect($host, $user, $password, $database);
-//gmygit mysqli_set_charset($connection, "utf8");
+//g mygit mysqli_set_charset($connection, "utf8");
 if(!$connection){
   echo "Error" . mysqli_connect_error();
   exit;
