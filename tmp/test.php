e4d6424136b4c854d054cc051b2285841c6b8a45
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-10-28 13:57:16 +0100

    phpmailer

--- tmp/test.php
+++ tmp/test.php
@@ -1,3 +1,24 @@
-<script>
-    window.location.replace("../my-account.php");
-</script>
+<?php
+    $to = "arthur.benbassat@gmail.com";
+    $subject = "Register your email";
+
+    $message = "
+    <html>
+    <head>
+    <title>Register your account</title>
+    </head>
+    <body>
+    <p>register your mail</p>
+    
+    <a href='http://arthur.6tib.be/login/registerMail.php>Click here</a>
+    </body>
+    </html>
+    ";
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: <arthur.benbassat@gmail.com>' . "\r\n";
+
+    mail($to,$subject,$message,$headers);
