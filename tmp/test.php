7dc7bc2a2838e3d0792032a9607d04e201b8376c
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-11-11 13:00:51 +0100

    opkuis

--- tmp/test.php
+++ tmp/test.php
@@ -1,24 +1,14 @@
 <?php
-    $to = "arthur.benbassat@gmail.com";
-    $subject = "Register your email";
-
-    $message = "
-    <html>
-    <head>
-    <title>Register your account</title>
-    </head>
-    <body>
-    <p>register your mail</p>
+include '../sql/dbconnection.php';
+$email2 = "ljqsdjqskljdsjdsmlkds";
+try {
+    $sql2 = "SELECT email FROM Customers WHERE email = $email2";
+    if (!mysqli_query($connection, $sql2)){
+        
+    }else {
+        throw new Exception("Incorrect username!");
+    }
     
-    <a href='http://arthur.6tib.be/login/registerMail.php>Click here</a>
-    </body>
-    </html>
-    ";
-
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-    // More headers
-    $headers .= 'From: <arthur.benbassat@gmail.com>' . "\r\n";
-
-    mail($to,$subject,$message,$headers);
+} catch (Exception $e) {
+    echo 'Your email is already known';
+ }
