6da63d116fa997ea1f1d4a2982fac35830e3fe57
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-11-17 17:28:50 +0100

    verbetering

--- tmp/test2.php
+++ tmp/test2.php
@@ -1,9 +1,103 @@
 <?php
 echo "<pre>";
-var_dump($_SERVER);
 $base_path = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 
 echo '<br>';
 echo str_replace(basename(__FILE__, '.php'), '../verify', $base_path); 
 echo "</pre>";
+
+
+
+/*
+    
+    // toon het command en de parameters
+    $errorObject = new stdClass();
+    $errorObject->error = 0;
+    $errorObject->errorDescription = '';
+    $errorObject->debug = "Het uit te voeren commando is: $tableCommand en de parameters zijn: " . implode(', ', $params) ;
+    echo json_encode($errorObject);
+
+try {
+    include "sql/dbconnection.php";
+    if (substr($_SERVER['PHP_SELF'], 13, 9) == "customers") {
+        // gebruikers
+        $tableName =  "Customers";
+        if (substr($_SERVER['PHP_SELF'], 23, 1) == "") {
+            $sql = "Select * from $tableName";
+        } else {
+            if ((substr($_SERVER['PHP_SELF'], 24, 1) == "") || (substr($_SERVER['PHP_SELF'], 24, 1) == "/")) {
+                $id =  substr($_SERVER['PHP_SELF'], 23, 1);
+                $sql = "Select * from $tableName where id = $id";
+            } else {
+                $id =  substr($_SERVER['PHP_SELF'], 23, 2);
+                $sql = "Select * from $tableName where id = $id";
+            }
+        }
+        $result = mysqli_query($connection, $sql);
+
+        if (!$result) {
+            throw new Exception('500');
+        }
+
+        $customers = [];
+        while ($rij = $result->fetch_assoc()) {
+            $customer = new stdClass();
+            $customer->id = $rij["id"];
+            $customer->customer_type_id = $rij["customer_type_id"];
+            $customer->email = $rij["email"];
+            $customer->first_name = $rij["first_name"];
+            $customer->last_name = $rij["last_name"];
+            $customer->address_line1 = $rij["address_line1"];
+            $customer->address_line2 = $rij["address_line2"];
+            $customer->postal_code = $rij["postal_code"];
+            $customer->city = $rij["city"];
+            $customer->country = $rij["country"];
+            $customer->phone_number = $rij["phone_number"];
+            $customer->organization_name = $rij["organization_name"];
+            $customer->vat_number = $rij["vat_number"];
+            $customers[] = $customer;
+        }
+        echo json_encode($customers);
+    } elseif (substr($_SERVER['PHP_SELF'], 13, 8) == "products") {
+        // Producten 
+        $tableName =  "Products";
+
+        if (substr($_SERVER['PHP_SELF'], 23, 1) == "") {
+            $sql = "Select * from $tableName";
+        } else {
+            if ((substr($_SERVER['PHP_SELF'], 24, 1) == "") || (substr($_SERVER['PHP_SELF'], 24, 1) == "/")) {
+                $id =  substr($_SERVER['PHP_SELF'], 23, 1);
+                $sql = "Select * from $tableName where id = $id";
+            } else {
+                $id =  substr($_SERVER['PHP_SELF'], 23, 2);
+                $sql = "Select * from $tableName where id = $id";
+            }
+        }
+
+        $result = mysqli_query($connection, $sql);
+
+        if (!$result) {
+            throw new Exception('500');
+        }
+
+        $products = [];
+        while ($rij = $result->fetch_assoc()) {
+            $product = new stdClass();
+            $product->id = $rij["id"];
+            $product->name = $rij["name"];
+            $product->description = $rij["description"];
+            $product->ingredients = $rij["ingredients"];
+            $product->category_id = $rij["category_id"];
+            $product->media_id = $rij["media_id"];
+
+            $products[] = $product;
+        }
+        echo json_encode($products);
+    } else {
+        echo 'we werken er aan sss';
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+}
+*/
