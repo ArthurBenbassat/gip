dda45037a27d05ae91576c391b76c305b5816139
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-01-12 22:30:44 +0100

    niks gedaan

--- tmp/test3.php
+++ tmp/test3.php
@@ -1,26 +1,10 @@
 <?php
 
-echo '<h1>Enkelvoudige variabele</h1>';
-$a = 'test';
-echo json_encode($a);
-
-echo '<h1>array van arrays</h1>';
-$a = array
-(
-array(23,"Cake en gebak"),
-array(6,"Droge koekjes"),
-);
-echo json_encode($a);
-
-echo '<h1>array van objecten</h1>';
-$a = [];
-$obj = new stdClass();
-$obj->id = 23;
-$obj->name = 'Cake en gebak';
-$a[] = $obj;
-$obj = new stdClass();
-$obj->id = 6;
-$obj->name = 'Droge koekjes';
-$a[] = $obj;
-echo json_encode($a);
+if (array_key_exists('cart', $_COOKIE)) {
+    echo 'Cart ID = ' . $_COOKIE['cart'];
+}
+else {
+    echo 'geen winkelmandje';
+    setcookie('cart', 12, time() + 10);
+}
 
