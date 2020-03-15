94219d6814ce5502e24f3754d76e85249c697ae6
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-03-15 22:47:51 +0100

    opkuis

--- product.php
+++ product.php
@@ -27,65 +27,14 @@ if(array_key_exists('id', $_GET)) {
         <div class="row s_product_inner">
           <div class="col-lg-6">
             <div class="s_product_img">
-              <div
-                id="carouselExampleIndicators"
-                class="carousel slide"
-                data-ride="carousel"
-              >
-                <ol class="carousel-indicators">
-                  <li
-                    data-target="#carouselExampleIndicators"
-                    data-slide-to="0"
-                    class="active"
-                  >
-                    <img
-                      src="img/product/single-product/s-product-s-2.jpg"
-                      alt=""
-                    />
-                  </li>
-                  <li
-                    data-target="#carouselExampleIndicators"
-                    data-slide-to="1"
-                  >
-                    <img
-                      src="img/product/single-product/s-product-s-3.jpg"
-                      alt=""
-                    />
-                  </li>
-                  <li
-                    data-target="#carouselExampleIndicators"
-                    data-slide-to="2"
-                  >
-                    <img
-                      src="img/product/single-product/s-product-s-4.jpg"
-                      alt=""
-                    />
-                  </li>
-                </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
+             
                     <img
                       class="d-block w-100"
-                      src="img/product/single-product/s-product-1.jpg"
+                      src="img/product/<?php echo $product->photo ?>"
                       alt="First slide"
                     />
-                  </div>
-                  <div class="carousel-item">
-                    <img
-                      class="d-block w-100"
-                      src="img/product/single-product/s-product-1.jpg"
-                      alt="Second slide"
-                    />
-                  </div>
-                  <div class="carousel-item">
-                    <img
-                      class="d-block w-100"
-                      src="img/product/single-product/s-product-1.jpg"
-                      alt="Third slide"
-                    />
-                  </div>
-                </div>
-              </div>
+
+
             </div>
           </div>
           <div class="col-lg-5 offset-lg-1">
