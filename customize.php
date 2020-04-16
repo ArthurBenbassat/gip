e9166353ecbc2868ac7d7a759a29e4dbb51ddb43
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-04-16 14:48:40 +0200

    bug

--- customize.php
+++ customize.php
@@ -10,27 +10,11 @@
       <?php
     require_once('snippets/header.html');
   ?>
-        <!--================Header Menu Area =================-->
-        <section class="banner_area">
-          <div class="banner_inner d-flex align-items-center">
-            <div class="container">
-              <div class="banner_content d-md-flex justify-content-between align-items-center">
-                <div class="mb-3 mb-md-0">
-                  <h2>Customize</h2>
-                </div>
-                <div class="page_link">
-                  <a href="index.php">Home</a>
-                  <a href="customize.php">customize</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+      
         <div class="container">
 
           <p>
-            Your wish list is empty
+            Hier wordt nog aan gewerkt!
           </p>
         </div>
 
