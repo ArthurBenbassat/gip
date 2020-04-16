4a385f1184b82224b459540910841ca317c80318
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-04-16 22:42:43 +0200

    test

--- order.php
+++ order.php
@@ -1,8 +1,6 @@
 <?php
 if (array_key_exists('guid', $_GET) && $_COOKIE['guid'] == $_GET['guid']) {
     
-    
-    
     echo '<!DOCTYPE html>';
     echo "<html lang='en'>";
 
@@ -15,7 +13,7 @@ if (array_key_exists('guid', $_GET) && $_COOKIE['guid'] == $_GET['guid']) {
         require_once 'snippets/header.html';
         
         echo '<div class="container"><article><h3>Thank you for your order at Benbassat, the order that you have placed is being processed. We have sent you the invoice of this order for confirmation.</h3></article></div>';
-        echo '<div class="container"><article style="margin-bottom: 10%"><button class="main_btn" onclick="\window.location.href = "index.php";\">Go back to the home page</button></article></div>';
+        echo '<div class="container"><article style="margin-bottom: 10%"><form action="index.php"><button class="main_btn" type="submit">Go back to the home page</button></form></article></div>';
         require_once 'snippets/footer.php';
 
         require_once 'snippets/js.html';
