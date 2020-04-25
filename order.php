5a243abd4b03ee9a731cbee14826560af98fdb90
Arthur Benbassat <arthur.benbassat@outlook.com>
2020-04-26 00:10:10 +0200

    testing

--- order.php
+++ order.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 if (array_key_exists('guid', $_GET) && $_COOKIE['guid'] == $_GET['guid']) {
     
     echo '<!DOCTYPE html>';
